894085ae1da661f1512cca563405d81331cf0d18
Arturo Cuevas <arturuscv@gmail.com>
2021-05-19 20:15:35 -0500

    Desafio-Utilizando Firebase

--- src/components/itemcount.js
+++ src/components/itemcount.js
@@ -1,26 +1,35 @@
 import React, { useState} from 'react';
+import {Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const ItemCount =({initial, stock ,onAdd})=>{
     const [count, setCount] = useState(initial);
-    const add=()=>{if((count<stock) && (stock>1)) setCount(count+1)}
+    const add=()=>{
+        if(count==stock){
+            handleClick(count);
+            
+        }else{
+           setCount(count+1)
+            console.log(count)
+        }
+
+
+        } 
     const remove=()=>{if(count<=stock && count>initial) setCount(count-1)}
     const handleClick =()=>onAdd(count)  
- const handleClick2=()=>{
-     alert('Ya no hay suficiente stock')
- }
+
 
     return(
 
     <div className="card text-white bg-primary" style={{maxWidth:'8rem', maxHeight:'10rem'}}>
-    <div className="card-body">
-    <p>{count}</p>
-    <button onClick={remove} className="btn btn-light">-</button>
-    <button onClick={add} className="btn btn-light">+</button>
-    </div>{
-       stock>0?<button  onClick={handleClick} className="btn btn-light">Add</button>:<button onClick={handleClick2} className="btn btn-light">Add</button>
-    }
-  
+    <div className="card-body"> 
+
+        <p>{count}</p>
+         <button onClick={remove} className="btn btn-light">-</button>
+    <button onClick={add} className="btn btn-light">+</button> 
+    <button  onClick={handleClick} className="btn btn-light">Add</button>
+    </div>
+
 
 </div>
         
